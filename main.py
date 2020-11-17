8d183e9386faf89caaea6d2d6c7bd4462fdf5465
Mohamed Boussakouk <m.boussakouk@student.bit-academy.nl>
2020-11-17 10:03:48 +0100

    Gitignore added & import change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import re
 from src.render import RenderResult
 from src.registery_manager import RegisteryManager
 
@@ -15,7 +14,8 @@ data = registery_manager.data
 if port_registrated == True:
     if not isinstance(data[user_input], list):
         items.append(
-            registery_manager.retrieve_port_info(data[user_input])
+            registery_manager.retrieve_port_info(
+                data[user_input])
             )
     else:
         for x in data[user_input]:
